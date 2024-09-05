bcb6d25fcd813d7c691877632cb8adc030f7a0de
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-09-05 17:04:17 +0300

    Equatable added

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:crypto_coins_list/repositories/crypto_coins/abstract_coins_repository.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'crypto_list_state.dart';
@@ -11,7 +12,9 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
   CryptoListBloc(this.coinsRepository) : super(CryptoListInitial()) {
     on<LoadCryptoList>((event, emit) async {
       try {
-        emit(CryptoListLoading());
+        if(state is! CryptoListLoaded) {
+          emit(CryptoListLoading());
+        }
         final coinsList = await coinsRepository.getCoinsList();
         emit(CryptoListLoaded(coinsList: coinsList));
       } catch (e) {
