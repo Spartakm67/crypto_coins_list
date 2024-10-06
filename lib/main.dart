86deef83120c6cd7e7908ad791de57419e4e4d0d
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-10-06 18:10:18 +0300

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'package:crypto_coins_list/cripto_coins_list_app.dart';
+// import 'package:crypto_coins_list/firebase_options.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -9,27 +11,34 @@ import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug('Talker started...');
+  
+  // final app = await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // talker.info(app.options.projectId);
 
   final dio = Dio();
   dio.interceptors.add(
     TalkerDioLogger(
-      talker: talker,
-      settings: const TalkerDioLoggerSettings(
-        printResponseData: false,
-      )),
-      );
+        talker: talker,
+        settings: const TalkerDioLoggerSettings(
+          printResponseData: false,          
+        )),
+  );
 
   Bloc.observer = TalkerBlocObserver(
-    talker: talker,
-    settings: const TalkerBlocLoggerSettings(
-      printStateFullData: false,
-      printEventFullData: false,
-    ));
+      talker: talker,
+      settings: const TalkerBlocLoggerSettings(
+        printStateFullData: false,
+        printEventFullData: false,        
+      )
+    );
 
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: dio));
